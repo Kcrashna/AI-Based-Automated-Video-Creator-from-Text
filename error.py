22716ccd8ede43947336821a89064a9d4813bc66
Rashna K.C. <rashna.kc10@gmail.com>
2024-10-07 21:33:22 +0545

    changes made

--- error.py
+++
@@ -1,3 +0,0 @@
-import torch
-print(torch.cuda.is_available())  # This should return True if CUDA is available
-print(torch.version.cuda)          # This shows the CUDA version PyTorch is compiled with
